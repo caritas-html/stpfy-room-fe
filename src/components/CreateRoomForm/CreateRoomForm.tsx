b849547410e9f840605f253dc2cd391182083e1e
caritas-html <leoexori@hotmail.com>
2023-12-22 15:14:21 -0300

    feat: add getRoom request

--- src/components/CreateRoomForm/CreateRoomForm.tsx
+++ src/components/CreateRoomForm/CreateRoomForm.tsx
@@ -1,35 +1,51 @@
 import { useState, FormEvent } from "react";
-import axios from "src/utils/http/axios"
+import axios from "src/utils/http/axios";
+// import { useNavigate } from "react-router-dom";
 
 export default function CreateRoomForm() {
   const [guestCanPause, setGuestCanPause] = useState(false);
   const [skipVotesCount, setSkipVotesCount] = useState(0);
 
-  const createRoomInstance = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  // const navigate = useNavigate();
 
+  const createRoomInstance = async () => {
     const data = {
       guest_can_pause: guestCanPause,
       votes_to_skip: skipVotesCount,
     };
 
     try {
-        const response = await axios.post("create-room", data)
-        if (!response) {
-            throw new Error("error request")
-        }
+      const response = await axios.post("create-room", data);
+      if (!response) {
+        throw new Error("error request");
+      }
 
-        return response.data;
+      return response.data;
     } catch (err) {
-        console.log(err)
+      console.log(err);
     }
+  };
+
+  const getRoom = async (roomCode: string) => {
+    try {
+      const response = axios.get(`/get-room?code=${roomCode}`);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleSubmitForm = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const room = await createRoomInstance();
+    await getRoom(room.code);
   };
 
   return (
     <div>
       <h3>Create Room</h3>
       <div>
-        <form onSubmit={createRoomInstance}>
+        <form onSubmit={handleSubmitForm}>
           <label>
             <input
               type="checkbox"
